31cb4bc705d925353d1445b0b0e65419170d49db
Codster177 <codster157@gmail.com>
2024-07-28 18:27:27 -0400

    7/28.2024 - Project Start
    I am starting this project in Qt. I will spend the first bit of this project learning the documentation of Qt as it is the first time I am using it.

---
+++ DND-Helper-Project/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_DND-Helper-Project_FILE C:/Users/codst/Documents/DND Helper Project/dnd-helper-project/DND-Helper-Project/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/DND-Helper-Project.exe)
+set(__QT_DEPLOY_TARGET_DND-Helper-Project_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_DND-Helper-Project_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
